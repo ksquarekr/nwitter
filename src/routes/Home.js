3bba4427b6584344e850d599401ddaa83fa5f629
Sungkwan Kang <ksquarekr@gmail.com>
2020-10-05 13:11:02 +0900

    After finishing section 3.6

---
+++ src/routes/Home.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { dbService } from "fbase";
+import Nweet from "components/Nweet";
+
+const Home = ({ userObj }) => {
+    const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    useEffect(() => {
+        dbService.collection("nweets").onSnapshot(snapshot => {
+            const nweetArray = snapshot.docs.map(doc => ({id:doc.id, ...doc.data()}));
+            setNweets(nweetArray);
+        });
+    }, []);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.collection("nweets").add({
+            text: nweet,
+            createdAt: Date.now(),
+            creatorId: userObj.uid,
+        });
+        setNweet("");
+    };
+    const onChange = (event) => {
+        const {target:{value}} = event;
+        setNweet(value);
+    };
+    console.log("userObj.uid:", userObj.uid);
+    console.log("nweets:", nweets);
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="file" accept="image/*" />
+                <input type="submit" value="Nweet" />
+            </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid} />
+                ))}
+            </div>
+        </div>
+    );
+};
+export default Home;
