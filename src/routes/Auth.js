9caac90b0fffe2a33a85b461127f0d25d58bf335
Sungkwan Kang <ksquarekr@gmail.com>
2020-10-06 12:00:00 +0900

    After finishing section 4.4

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -46,7 +46,7 @@ const Auth = () => {
         <div>
             <form onSubmit={onSubmit}>
                 <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
-                <input name="password" type="text" placeholder="Password" required value={password} onChange={onChange} />
+                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
                 <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
                 {error}
             </form>
