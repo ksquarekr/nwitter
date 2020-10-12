432377bf034f4d13cadb97a0de9757551e1db045
Sungkwan Kang <ksquarekr@gmail.com>
2020-10-12 14:30:21 +0900

    Updates

---
+++ precache-manifest.7b5c1a4fc685f5af010231ef4b41bed9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904260b980abc72001c3826b0096c719",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "bf7d7bf7ec77f22c3d13",
+    "url": "/nwitter/static/css/main.ff7f2547.chunk.css"
+  },
+  {
+    "revision": "0cbe583cd7e9387e29e8",
+    "url": "/nwitter/static/js/2.2a0d41a6.chunk.js"
+  },
+  {
+    "revision": "7e320732ce8e269e8d356159c55ade0e",
+    "url": "/nwitter/static/js/2.2a0d41a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7d7bf7ec77f22c3d13",
+    "url": "/nwitter/static/js/main.1a27ef9d.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
